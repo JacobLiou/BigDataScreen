c30102667be49630081d1291d57690998e800238
liumenghui <liumenghui@sofarsolar.com>
2024-08-14 14:51:52 +0800

    chore: 去除执行目录 临时目录

--- BigScreenDataShow/obj/Debug/net6.0-windows/BigScreenDataShow.AssemblyInfo.cs
+++ BigScreenDataShow/obj/Debug/net6.0-windows/BigScreenDataShow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BigScreenDataShow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20701d1575d11f0cd9ad8bd993a5b1196ce16ad7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BigScreenDataShow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BigScreenDataShow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
