975dfe010ddc893ffcd0432a889e52bf661e1c08
chenguanjie <chenguanjie@sofarsolar.com>
2024-10-15 19:40:07 +0800

    使用FluentScheduler进行特定时间点任务调度,调整大屏程序启动时数据更新

--- BigScreenDataShow/ViewModels/MainWindowViewModel.cs
+++ BigScreenDataShow/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using BigScreenDataShow.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using FluentScheduler;
 using LiveChartsCore;
 using LiveChartsCore.Defaults;
 using LiveChartsCore.Measure;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Extensions;
 using LiveChartsCore.SkiaSharpView.Painting;
+using Microsoft.Win32;
 using Newtonsoft.Json;
 using SkiaSharp;
 using System;
@@ -99,33 +102,37 @@ namespace BigScreenDataShow.ViewModels
 
         private Random random = new Random();
 
+
+        private FluentScheduler.Registry registry = new FluentScheduler.Registry();
         public MainWindowViewModel()
         {
             InitAging_hoursharing_yield();
             InitAging_daysharing_yield();
+
+
             //四个站工位
-            _timer = new DispatcherTimer();
-            _timer.Interval = new TimeSpan(0, 10, 0);
-            _timer.Tick += _timer_Tick;
-            _timer.Start();
+            //_timer = new DispatcherTimer();
+            //_timer.Interval = new TimeSpan(0, 10, 0);
+            //_timer.Tick += _timer_Tick;
+            //_timer.Start();
 
             //老化产量
-            _agingtimer = new DispatcherTimer();
-            _agingtimer.Interval = new TimeSpan(1, 0, 0);
-            _agingtimer.Tick += _agingtimer_Tick;
-            _agingtimer.Start();
+            //_agingtimer = new DispatcherTimer();
+            //_agingtimer.Interval = new TimeSpan(1, 0, 0);
+            //_agingtimer.Tick += _agingtimer_Tick;
+            //_agingtimer.Start();
 
             //老化分时产量
-            _aginghourtimer = new DispatcherTimer();
-            _aginghourtimer.Interval = new TimeSpan(0, 0, 2);
-            _aginghourtimer.Tick += _aginghourtimer_Tick;
-            _aginghourtimer.Start();
+            //_aginghourtimer = new DispatcherTimer();
+            //_aginghourtimer.Interval = new TimeSpan(0, 0, 2);
+            //_aginghourtimer.Tick += _aginghourtimer_Tick;
+            //_aginghourtimer.Start();
 
             //老化日产量趋势
-            _agingdaytimer = new DispatcherTimer();
-            _agingdaytimer.Interval = new TimeSpan(4, 0, 0);
-            _agingdaytimer.Tick += _agingdaytimer_Tick;
-            _agingdaytimer.Start();
+            //_agingdaytimer = new DispatcherTimer();
+            //_agingdaytimer.Interval = new TimeSpan(0, 0, 2);
+            //_agingdaytimer.Tick += _agingdaytimer_Tick;
+            //_agingdaytimer.Start();
 
             _updaterealtimetimer = new DispatcherTimer();
             _updaterealtimetimer.Interval = new TimeSpan(0, 0, 1);
@@ -135,15 +142,63 @@ namespace BigScreenDataShow.ViewModels
             _httpClient = new HttpClient();
             _httpClient.Timeout = TimeSpan.FromSeconds(60);
 
+            Registry_Start();
+
+            RefreshAginghourData(true);
+            RefreshAgingdayData(true);
+
+
             Task.Run(async () =>
             {
                 await Task.Delay(1000);
                 _updaterealtimetimer_Tick(null, null);
-                _timer_Tick(null, null);
-                _agingtimer_Tick(null, null);
-                _aginghourtimer_Tick(null, null);
-                _agingdaytimer_Tick(null, null);
+                //_timer_Tick(null, null);
+                //_agingtimer_Tick(null, null);
+                //_aginghourtimer_Tick(null, null);
+                //_agingdaytimer_Tick(null, null);
             });
+        }
+
+        private void Registry_Start()
+        {
+            #region 调试用
+            //每隔2s任务（调试模拟用）
+            //registry.Schedule(() => { _agingtimer_Tick(null, null); }).NonReentrant().ToRunNow().AndEvery(1).Minutes();
+            //registry.Schedule(() => { ResetChartAginghourData(); }).NonReentrant().ToRunNow().AndEvery(2).Seconds();
+            //registry.Schedule(() => { ResetChartAgingdayData(); }).NonReentrant().ToRunNow().AndEvery(2).Seconds();
+            #endregion
+
+
+            //每隔10min任务——查询H3线耐压,T1,T2,包装良率，更新饼图
+            registry.Schedule(() => { _timer_Tick(null, null); }).NonReentrant().ToRunNow().AndEvery(10).Minutes();
+            //每隔1h任务——查询H3线G3,G4,HBI机型老化通过数，更新文本
+            registry.Schedule(() => { _agingtimer_Tick(null, null); }).NonReentrant().ToRunNow().AndEvery(1).Hours();
+            //每隔1h间隔，每个小时的第50分钟任务——查询H3线G3,G4,HBI机型老化通过数之和(1小时)，更新柱形图
+            registry.Schedule(() => { _aginghourtimer_Tick(null, null); }).NonReentrant().ToRunEvery(1).Hours().At(50);
+            //每隔1d间隔，每个天数的23:55任务——查询H3线G3,G4,HBI机型老化通过数之和(1天)，更新折线图
+            registry.Schedule(() => { _agingdaytimer_Tick(null, null); }).NonReentrant().ToRunEvery(1).Days().At(23, 55);
+
+            //每天0点任务——重置老化分时产量柱形图表
+            registry.Schedule(() => { ResetChartAginghourData(); }).NonReentrant().ToRunEvery(0).Days().At(0, 0);
+            //每月1号0时0分任务——重置老化日产量趋势折线图表
+            registry.Schedule(() => { ResetChartAgingdayData(); }).NonReentrant().ToRunEvery(0).Months().On(1).At(0, 0);
+            JobManager.Initialize(registry);
+        }
+
+        private void ResetChartAginghourData()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                aging_hoursharing_data[i].Value = 0;
+            }
+        }
+
+        private void ResetChartAgingdayData()
+        {
+            for (int i = 0; i < 31; i++)
+            {
+                aging_daysharing_data[i].Value = 0;
+            }
         }
 
         /// <summary>
@@ -271,9 +326,6 @@ namespace BigScreenDataShow.ViewModels
         /// <param name="e"></param>
         private void _aginghourtimer_Tick(object sender, EventArgs e)
         {
-            string startdate = DateTime.Today.ToString("yyyy-MM-dd HH:mm:ss");
-            string enddate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-
             Task.Run(() => RefreshAginghourData());
         }
 
@@ -284,9 +336,6 @@ namespace BigScreenDataShow.ViewModels
         /// <param name="e"></param>
         private void _agingdaytimer_Tick(object sender, EventArgs e)
         {
-            string startdate = DateTime.Today.ToString("yyyy-MM-dd HH:mm:ss");
-            string enddate = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-
             Task.Run(() => RefreshAgingdayData());
         }
 
@@ -333,7 +382,48 @@ namespace BigScreenDataShow.ViewModels
             #endregion
         }
 
-        private void RefreshAginghourData()
+        private int GetAgingData(string startdate, string enddate)
+        {
+            string g3 = "0";
+            string g4 = "0";
+            string ebi = "0";
+            string agingtablecategory1 = "G3";
+            string agingtablecategory2 = "G4";
+            string agingtablecategory3 = "EBI";
+            string url1 = $@"{_rootUrl}/GetAgingPassData?agingtablecategory={agingtablecategory1}&startdate={startdate}&enddate={enddate}";
+            string url2 = $@"{_rootUrl}/GetAgingPassData?agingtablecategory={agingtablecategory2}&startdate={startdate}&enddate={enddate}";
+            string url3 = $@"{_rootUrl}/GetAgingPassData?agingtablecategory={agingtablecategory3}&startdate={startdate}&enddate={enddate}";
+            try
+            {
+                var response = _httpClient.GetAsync(url1).GetAwaiter().GetResult();
+                if (response != null && response.IsSuccessStatusCode)
+                {
+                    g3 = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                response = _httpClient.GetAsync(url2).GetAwaiter().GetResult();
+                if (response != null && response.IsSuccessStatusCode)
+                {
+                    g4 = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                response = _httpClient.GetAsync(url3).GetAwaiter().GetResult();
+                if (response != null && response.IsSuccessStatusCode)
+                {
+                    ebi = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+            }
+            catch
+            {
+            }
+            return int.Parse(g3) + int.Parse(g4) + int.Parse(ebi);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstFlag">第一次读取标志</param>
+        private void RefreshAginghourData(bool firstFlag = false)
         {
             #region 模拟数据
             //for (int i = 0; i < aging_hoursharing_data.Count();i++)
@@ -343,9 +433,30 @@ namespace BigScreenDataShow.ViewModels
             //    lastInstance.Value = randomValue;
             //}
             #endregion
+
+            #region 正式逻辑
+            DateTime dateTime = DateTime.Today;
+            int hour = DateTime.Now.Hour;
+            string startdate = dateTime.ToString("yyyy-MM-dd") + $" {hour}:00:00";
+            string enddate = dateTime.ToString("yyyy-MM-dd") + $" {hour}:59:59";
+
+            if (firstFlag)
+            {
+                for(int i = 0;i < hour + 1;i++)
+                {
+                    startdate = dateTime.ToString("yyyy-MM-dd") + $" {i.ToString("00")}:00:00";
+                    enddate = dateTime.ToString("yyyy-MM-dd") + $" {i.ToString("00")}:59:59";
+                    aging_hoursharing_data[i].Value = GetAgingData(startdate, enddate);
+                }
+            }
+            else
+                aging_hoursharing_data[hour].Value = GetAgingData(startdate, enddate);
+
+            #endregion
         }
 
-        private void RefreshAgingdayData()
+
+        private void RefreshAgingdayData(bool firstFlag = false)
         {
             #region 模拟数据
             //for (int i = 0; i < aging_daysharing_data.Count(); i++)
@@ -354,6 +465,25 @@ namespace BigScreenDataShow.ViewModels
             //    var lastInstance = aging_daysharing_data[i];
             //    lastInstance.Value = randomValue;
             //}
+            #endregion
+
+            #region 正式逻辑
+            DateTime dateTime = DateTime.Today;
+            int day = DateTime.Now.Day;
+            string startdate = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+            string enddate = dateTime.ToString("yyyy-MM-dd") + $" 23:59:59";
+
+            if (firstFlag)
+            {
+                for(int i = 1; i <= day;i++)
+                {
+                    startdate = dateTime.ToString("yyyy-MM") + $"-{i.ToString("00")} 00:00:00";
+                    enddate = dateTime.ToString("yyyy-MM") + $"-{i.ToString("00")} 23:59:59";
+                    aging_daysharing_data[i - 1].Value = GetAgingData(startdate, enddate);
+                }
+            }
+            else
+                aging_daysharing_data[day - 1].Value = GetAgingData(startdate, enddate);
             #endregion
         }
 
@@ -522,6 +652,12 @@ namespace BigScreenDataShow.ViewModels
                     action();
                 }
             }, null);
+        }
+
+        [RelayCommand]
+        private void Unloaded()
+        {
+            JobManager.Stop();
         }
     }
 }
